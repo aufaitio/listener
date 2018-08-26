daf5f7fb918acb5c7b5a0b8b7de79d47c5119c7b
Andy Gertjejansen <andy.gertjejansen@bestbuy.com>
2018-08-25 21:16:14 -0500

    Run go builds at same time

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 dir="${0%/*}"
+pids=()
 
 for os in "linux" "windows" "darwin"; do
 	mkdir -p "$dir/../builds/$os"
-	(cd "$dir/../" || exit; GOOS="$os" GOARCH=amd64 make build)
+	(cd "$dir/../" || exit; GOOS="$os" GOARCH=amd64 make build) &
+	pids[${os}]=$!
+done
+
+for pid in ${pids[*]}; do
+	wait "$pid"
 done
