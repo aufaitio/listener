43597e3177eb9986032db0157aa0d34fc4fea76d
Andy Gertjejansen <andy.gertjejansen@bestbuy.com>
2018-08-25 21:08:55 -0500

    Update build script so it can be run anywhere

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-
-mkdir -p builds/linux builds/window builds/darwin
+dir="${0%/*}"
 
 for os in "linux" "windows" "darwin"; do
-    GOOS="$os" GOARCH=amd64 make build
+	mkdir -p "$dir/../builds/$os"
+	(cd "$dir/../" || exit; GOOS="$os" GOARCH=amd64 make build)
 done
